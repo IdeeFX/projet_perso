677402b523112f41908bab706d1a00d450dd7d14
François-Xavier <francois-xavier.davanne@meteo.fr>
2018-07-16 14:57:44 +0200

    Première implémentation du WSDL

--- testFlask/apps/spyned.py
+++ testFlask/apps/spyned.py
@@ -1,30 +1,101 @@
 from spyne.application import Application
 from spyne.decorator import srpc
-from spyne.model.complex import Iterable, ComplexModel
-from spyne.model.primitive import Integer, Unicode
+from spyne.model.complex import ComplexModel
+from spyne.model.primitive import Integer, Unicode, Boolean
 # from spyne.protocol.http import HttpRpc
 # from spyne.protocol.json import JsonDocument
 from spyne.protocol.soap import Soap11
 from spyne.service import ServiceBase
 from spyne.server.wsgi import WsgiApplication
 
-class TestComplex(ComplexModel):
+MailDispatchMode = Unicode(values=['TO', 'CC', 'BCC'])
+MailAttachmentMode = Unicode(values=['EMBEDDED_IN_BODY', 'AS_ATTACHMENT'])
+requestStatus = Unicode(values=['ONGOING_DISSEMINATION', 'DISSEMINATED', 'FAILED'])
+
+
+class Diffusion(ComplexModel):
 
     __namespace__="http://dissemination.harness.openwis.org/"
 
-    var1 = Unicode
-    var2 = Unicode
+    fileName = Unicode
+
+class FTPDiffusion(Diffusion):
+
+    __namespace__="http://dissemination.harness.openwis.org/"
+
+    host = Unicode
+    port = Unicode
+    user = Unicode
+    password = Unicode
+    passive = Boolean
+    remotePath = Unicode
+    checkFileSize = Boolean
+
+class MailDiffusion(Diffusion):
+
+    __namespace__="http://dissemination.harness.openwis.org/"
+
+    address = Unicode
+    headerLine = Unicode
+    subject = Unicode
+    dispatchMode = MailDispatchMode
+    attachmentMode = MailAttachmentMode
 
 
-class Dissemination(ServiceBase):
-    @srpc(Unicode, Unicode, TestComplex, _returns=Unicode)
+class DisseminationStatus(ComplexModel):
+
+    __namespace__="http://dissemination.harness.openwis.org/"
+
+    requestId = Unicode
+    requestStatus = requestStatus
+    message = Unicode
+
+    def __init__(self,  requestId, requestStatus, message):
+        # don't forget to call parent class initializer
+        super(DisseminationStatus, self).__init__()
+        self.requestId = requestId
+        self.requestStatus = requestStatus
+        self.message = message 
+
+class DisseminateResponse(ComplexModel):
+
+    __namespace__="http://dissemination.harness.openwis.org/"
+
+    disseminationResult = DisseminationStatus
+
+    def __init__(self,  disseminationResult):
+        # don't forget to call parent class initializer
+        super(DisseminateResponse, self).__init__()
+        self.disseminationResult = disseminationResult
+
+
+
+
+class DisseminationInfo(ComplexModel):
+
+    __namespace__="http://dissemination.harness.openwis.org/"
+
+    priority = Integer
+    SLA = Integer
+    dataPolicy = Unicode
+    diffusion = Diffusion
+    alternativeDiffusion = Diffusion
+
+
+class DisseminationService(ServiceBase):
+    @srpc(Unicode, Unicode, DisseminationInfo, _returns=DisseminateResponse)
     def disseminate(requestId, fileURI, testObject):
-        print(testObject.var1, testObject.var2)
-        return "{} {}".format(requestId, fileURI)
+        print(testObject.priority, testObject.SLA)
+
+        status = DisseminationStatus("reqId_1234", 'ONGOING_DISSEMINATION', "mess_hello")
+        dissResp = DisseminateResponse(status)
+
+
+        return dissResp
 
 
 application = Application(
-    [Dissemination], 'http://dissemination.harness.openwis.org/',
+    [DisseminationService], 'http://dissemination.harness.openwis.org/',
     # The input protocol is set as HttpRpc to make our service easy to call.
     # in_protocol=HttpRpc(validator='soft'),
     # out_protocol=JsonDocument(ignore_wrappers=True),
