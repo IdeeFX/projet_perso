11364ff2a921f3063ca27017ce975c399b3f718b
François-Xavier <francois-xavier.davanne@meteo.fr>
2018-10-01 15:47:35 +0200

    Bug fix for ftp file name

--- file_manager/manager.py
+++ file_manager/manager.py
@@ -671,13 +671,14 @@ class DiffMetManager:
                 etree.SubElement(element, prefix + "ftp_use_size").text = binBool(diff_info["checkFileSize"])
                 etree.SubElement(element, prefix + "ftp_passive").text = binBool(diff_info["passive"])
                 etree.SubElement(element, prefix + "ftp_port").text = self._get_port_value(diff_info)
+                etree.SubElement(element, prefix + "ftp_method_name").text = "NAME"
                 if diff_info["fileName"] != "":
                     etree.SubElement(element, prefix + "ftp_final_file_name").text = str(diff_info["fileName"])
                     etree.SubElement(element, prefix + "ftp_tmp_file_name").text = str(diff_info["fileName"]+ ".tmp")
                 else:
                     etree.SubElement(element, prefix + "ftp_final_file_name").text = self.original_filename
                     etree.SubElement(element, prefix + "ftp_tmp_file_name").text = self.original_filename + ".tmp"
-                etree.SubElement(element, "switch_method_medias_ftp").text = "NTRY"
+                # etree.SubElement(element, "switch_method_medias_ftp").text = "NTRY"
 
 
             elif diff_info["DiffusionType"] == "EMAIL":
